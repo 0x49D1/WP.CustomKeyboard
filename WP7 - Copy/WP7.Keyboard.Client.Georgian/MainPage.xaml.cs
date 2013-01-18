e1e5f67e89e827071b6d634565e56597848f52fc
Dmitry Pursanov <dpursanov@live.com>
2013-01-18 14:41:16 +0400

    WP keyboard another version try to 8

---
+++ WP7 - Copy/WP7.Keyboard.Client.Georgian/MainPage.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Security;
+using System.Windows;
+using BugSense;
+using Microsoft.Phone.Shell;
+using WP7.Keyboard.Essential;
+
+namespace WP7.Keyboard.Client
+{
+    public partial class MainPage : StatefulPhoneApplicationPage
+    {
+        private Keyboard.Controls.Keyboard geoKeyboard;
+        private Keyboard.Controls.Keyboard latinKeyboard;
+        private string localClipboard = string.Empty;
+        private bool isFirstLoad = true;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Keyboard.
+            latinKeyboard = new LatinKeyboard.LatinKeyboard();
+            geoKeyboard = this.Keyboard.Keyboard;
+        }
+
+        //protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedTo(e);
+        //}
+
+        //protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        //{
+        //    base.OnNavigatedFrom(e);
+
+        //    PreserveOutputControlState(this.Keyboard.OutputControl);
+        //    PreserveControlState((TextBox)this.FindName("PART_ScreenTextBox"));
+        //    PreserveFocusState(this.Keyboard);
+        //}
+
+        private void EmailButtonClick(object sender, EventArgs e)
+        {
+            MultimediaUtility.SendEmail(Keyboard.Keyboard.Visibility == Visibility.Visible
+                                            ? this.Keyboard.OutputControl.Text
+                                            : this.Keyboard.OutputReadControl.Text);
+        }
+
+        private void BingSearchButtonClick(object sender, EventArgs e)
+        {
+            MultimediaUtility.BingSearch(Keyboard.Keyboard.Visibility == Visibility.Visible
+                                             ? this.Keyboard.OutputControl.Text
+                                             : this.Keyboard.OutputReadControl.Text);
+        }
+
+        private void SmsButtonClick(object sender, EventArgs e)
+        {
+            MultimediaUtility.SendSms(Keyboard.Keyboard.Visibility == Visibility.Visible
+                                          ? this.Keyboard.OutputControl.Text
+                                          : this.Keyboard.OutputReadControl.Text);
+        }
+
+        private void ChangeKeyboardButtonClick(object sender, EventArgs e)
+        {
+            ApplicationBarMenuItem changeKeyboardButton = sender as ApplicationBarMenuItem;
+            if (changeKeyboardButton == null)
+            {
+                return;
+            }
+
+            if (ReferenceEquals(geoKeyboard, this.Keyboard.Keyboard))
+            {
+                this.Keyboard.Keyboard = this.latinKeyboard;
+                changeKeyboardButton.Text = "Switch to Georgian";
+            }
+            else
+            {
+                this.Keyboard.Keyboard = this.geoKeyboard;
+                changeKeyboardButton.Text = "Switch to English";
+            }
+        }
+
+        private void CopyTextButtonClick(object sender, EventArgs e)
+        {
+            try
+            {
+                localClipboard = this.Keyboard.Keyboard.Visibility == Visibility.Visible ? this.Keyboard.OutputControl.Text : this.Keyboard.OutputReadControl.Text;
+
+                Clipboard.SetText(localClipboard);
+                ApplicationBarMenuItem item = (ApplicationBarMenuItem)ApplicationBar.MenuItems[1];
+                if (item != null)
+                    item.IsEnabled = true;
+
+                MessageBox.Show("Success!");
+            }
+            catch (SecurityException ex)
+            {
+                BugSenseHandler.Instance.LogError(ex);
+            }
+            catch (Exception ex)
+            {
+                BugSenseHandler.Instance.LogError(ex);
+            }
+        }
+
+        private void PasteClick(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                if (this.Keyboard.Keyboard.IsKeyboardVisible)
+                    this.Keyboard.OutputControl.AppendToText(localClipboard);
+                else
+                    this.Keyboard.OutputReadControl.AppendToText(localClipboard);
+
+            }
+        }
+
+        private void ClearClick(object sender, EventArgs e)
+        {
+            if ((this.Keyboard.OutputControl.Text.Length > 0 && Keyboard.Keyboard.Visibility == Visibility.Visible)
+                || (this.Keyboard.OutputReadControl.Text.Length > 0 && Keyboard.Keyboard.Visibility != Visibility.Visible))
+            {
+                if (MessageBox.Show("Do you really want to clear all text?", "", MessageBoxButton.OKCancel) == MessageBoxResult.OK)
+                {
+                    try
+                    {
+                        if (Keyboard.Keyboard.Visibility == Visibility.Visible)
+                            this.Keyboard.OutputControl.Clear();
+                        else
+                            this.Keyboard.OutputReadControl.Clear();
+                    }
+                    catch (Exception ex)
+                    {
+                        BugSenseHandler.Instance.LogError(ex);
+                    }
+                }
+            }
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/YourLastAboutDialog;component/AboutPage.xaml", UriKind.Relative));
+        }
+    }
+}
---
+++ WP7 - Copy/WP7.LatinKeyboard/LatinKeyboard.cs
@@ -0,0 +1,49 @@
+﻿using WP7.Keyboard.KeyboardMapping;
+
+namespace WP7.LatinKeyboard
+{
+    public class LatinKeyboard : Keyboard.Controls.Keyboard
+    {
+        protected override KeyboardContext GenerateKeyboardContext()
+        {
+            KeyboardContext keyboardContext = new KeyboardContext();
+            keyboardContext.Rows = 3;
+
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "q"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "w"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "e"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "r"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "t"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "y"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "u"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "i"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "o"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(0, "p"));
+
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "a"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "s"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "d"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "f"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "g"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "h"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "j"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "k"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(1, "l"));
+
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "z"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "x"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "c"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "v"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "b"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "n"));
+            keyboardContext.KeyboardMapping.Add(new KeyMapping(2, "m"));
+
+            return keyboardContext;
+        }
+
+        protected override KeyboardContext GenerateSecondaryKeyboardContext()
+        {
+            return new DefaultSecondaryKeyboardContext();
+        }
+    }
+}
